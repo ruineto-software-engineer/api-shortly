5724ea37effcb9903a7b22f75f72e356753e7a21
Rui Neto <ruineto11@gmail.com>
2022-03-17 20:13:54 -0300

    feat: GET /urls/:shortUrl

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -25,3 +25,28 @@ export async function postUrl(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getShortUrl(req, res) {
+  const shortUrl = req.params.shortUrl;
+  console.log(shortUrl);
+
+  try {
+    if(!shortUrl){
+      res.sendStatus(404);
+      return;
+    }
+
+    const searchedShortUrl = await connection.query(`
+      SELECT * FROM "shortenedUrls" AS s WHERE s."shortUrl"=$1
+    `, [shortUrl]);
+    if(searchedShortUrl.rowCount === 0){
+      res.sendStatus(404);
+      return;
+    }
+
+    res.status(200).send(searchedShortUrl.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { postUrl } from "../controllers/urlsController.js";
+import { getShortUrl, postUrl } from "../controllers/urlsController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
 
 const urlRouter = Router();
-urlRouter.use(validateTokenMiddleware);
-urlRouter.post('/urls/shorten', validateSchemaMiddleware(urlSchema), postUrl);
+urlRouter.post('/urls/shorten', validateTokenMiddleware, validateSchemaMiddleware(urlSchema), postUrl);
+urlRouter.get('/urls/:shortUrl', getShortUrl);
 export default urlRouter;
