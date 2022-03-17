3a9cee7d5d2af9f3d7f4f64bf7fffffb74acfae1
Rui Neto <ruineto11@gmail.com>
2022-03-17 20:36:49 -0300

    feat: DELETE /urls/:id

--- src/controllers/urlsController.js
+++ src/controllers/urlsController.js
@@ -28,7 +28,6 @@ export async function postUrl(req, res) {
 
 export async function getShortUrl(req, res) {
   const shortUrl = req.params.shortUrl;
-  console.log(shortUrl);
 
   try {
     if(!shortUrl){
@@ -37,7 +36,8 @@ export async function getShortUrl(req, res) {
     }
 
     const searchedShortUrl = await connection.query(`
-      SELECT * FROM "shortenedUrls" AS s WHERE s."shortUrl"=$1
+      SELECT * FROM "shortenedUrls" AS s 
+      WHERE s."shortUrl"=$1
     `, [shortUrl]);
     if(searchedShortUrl.rowCount === 0){
       res.sendStatus(404);
@@ -50,3 +50,30 @@ export async function getShortUrl(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function deleteUrl(req, res) {
+  const user = res.locals.user;
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    const searchedShortUrl = await connection.query(`
+      SELECT * FROM "shortenedUrls" AS s 
+      WHERE s.id=$1 AND s."userId"=$2
+    `, [id, user.id]);
+    if(searchedShortUrl.rowCount === 0){
+      res.sendStatus(401);
+      return;
+    }
+
+    await connection.query(`
+      DELETE FROM "shortenedUrls" AS s 
+      WHERE s.id=$1;
+    `, [id]);
+
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+};
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getShortUrl, postUrl } from "../controllers/urlsController.js";
+import { deleteUrl, getShortUrl, postUrl } from "../controllers/urlsController.js";
 import { validateSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 import urlSchema from "../schemas/urlSchema.js";
@@ -7,4 +7,5 @@ import urlSchema from "../schemas/urlSchema.js";
 const urlRouter = Router();
 urlRouter.post('/urls/shorten', validateTokenMiddleware, validateSchemaMiddleware(urlSchema), postUrl);
 urlRouter.get('/urls/:shortUrl', getShortUrl);
+urlRouter.delete('/urls/:id', validateTokenMiddleware, deleteUrl);
 export default urlRouter;
